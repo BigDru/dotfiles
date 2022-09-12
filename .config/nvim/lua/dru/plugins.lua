208e0a0219d3159bf1bfe154be5bbbddd2deedf5
BigDru <al.dumbrava@gmail.com>
2022-09-12 00:01:33 -0400

    WIP

--- .config/nvim/lua/dru/plugins.lua
+++ .config/nvim/lua/dru/plugins.lua
@@ -1,3 +1,11 @@
+-- Note: requires nvim 0.7.0 or greater
+vim.api.nvim_create_autocmd('BufWritePost', 
+{
+    group = vim.api.nvim_create_augroup('packer_user_config', { clear = true }),
+    pattern = 'plugins.lua',
+    command = 'source <afile> | PackerSync'
+})
+
 -- bootstrap packer
 local ensure_packer = function()
   local fn = vim.fn
@@ -11,7 +19,7 @@ local ensure_packer = function()
         'https://github.com/wbthomason/packer.nvim', 
         install_path
     })
-    vim.cmd [[packadd packer.nvim]]
+    vim.api.nvim_command('packadd packer.nvim')
     return true
   end
   return false
@@ -20,36 +28,45 @@ end
 local packer_bootstrap = ensure_packer()
 -- !bootstrap packer
 
--- Note: requires nvim 0.7.0 or greater
-vim.api.nvim_create_autocmd('BufWritePost', 
-{
-    group = vim.api.nvim_create_augroup('packer_user_config', { clear = true }),
-    pattern = 'plugins.lua',
-    command = 'source <afile> | PackerCompile'
+function get_setup(name)
+    return string.format('require(\'dru/setup/%s\')', name)
+end
+
+return require('packer').startup({
+    function(use)
+        use('wbthomason/packer.nvim') -- Packer manages itself
+
+        -- May be used by other plugins:
+        -- use 'nvim-lua/popup.nvim' -- Implementation of Popup API 
+        -- use 'nvim-lua/plenary.nvim' -- Useful lua functions used in lots of plugins
+
+        -- lualine
+        use
+        {
+            'nvim-lualine/lualine.nvim',
+            config = get_setup('lualine'),
+            event = 'VimEnter',
+            requires = 
+            { 
+                'kyazdani42/nvim-web-devicons', 
+                opt = true,
+            },
+        }
+
+        use 'RRethy/nvim-base16'
+
+        if packer_bootsrap then
+            require('packer').sync()
+        end
+    end,
+    config = 
+    {
+        display = {
+            open_fn = require('packer.util').float,
+        },
+        profile = {
+            enable = true,
+            threshold = 1, -- the amount in ms that a plugin's loat time must be over for it to be included in the profile
+        }
+    }
 })
-
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
-
-    -- lualine
-    --use 
-    --{
-        --'nvim-lualine/lualine.nvim',
-        --event = 'VimEnter',
-        --config = function()
-            --require('lualine').setup()
-        --end,
-        --requires = 
-        --{ 
-            --'kyazdani42/nvim-web-devicons', 
-            --opt = true ,
-        --},
-    --}
-
-    use 'RRethy/nvim-base16'
-
-    if packer_bootsrap then
-        require('packer').sync()
-    end
-end)
-
