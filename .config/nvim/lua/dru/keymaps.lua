e9ce195199b07d491e8ee9382384693c50e7729e
Alexandru Dumbrava <al.dumbrava@gmail.com>
2022-10-23 01:20:00 -0400

    Added better window navigation and some additional comments for keymaps

--- .config/nvim/lua/dru/keymaps.lua
+++ .config/nvim/lua/dru/keymaps.lua
@@ -1,4 +1,5 @@
 local opts = { noremap = true, silent = true }
+-- noremap = no recurse map
 
 vim.api.nvim_set_keymap("", "<Space>", "<Nop>", opts)
 
@@ -7,15 +8,26 @@ vim.g.maplocalleader = " "
 
 vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 
+-- Normal --
+-- Better window navigation
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", opts)
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", opts)
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", opts)
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", opts)
+
 -- Resize with arrows
-vim.api.nvim_set_keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-vim.api.nvim_set_keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-vim.api.nvim_set_keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-vim.api.nvim_set_keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+vim.api.nvim_set_keymap("n", "<C-Up>", ":resize +2<CR>", opts)              -- increase size
+vim.api.nvim_set_keymap("n", "<C-Down>", ":resize -2<CR>", opts)            -- decrease size
+vim.api.nvim_set_keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)   -- increase size
+vim.api.nvim_set_keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)  -- decrease size
+
+-- Navigate buffers
+--vim.api.nvim_set_keymap("n", "<S-l>", ":bnext<CR>", opts)
+--vim.api.nvim_set_keymap("n", "<S-h>", ":bprev<CR>", opts)
 
 -- Insert
 -- jk fast to enter
-vim.api.nvim_set_keymap("i", "jk", "<esc>", opts)
+vim.api.nvim_set_keymap("i", "jk", "<ESC>", opts)
 
 -- Visual --
 -- Stay in indent mode
@@ -25,6 +37,9 @@ vim.api.nvim_set_keymap("v", ">", ">gv", opts)
 -- Move text up and down
 vim.api.nvim_set_keymap("v", "<A-j>", ":m .+1<CR>==", opts)
 vim.api.nvim_set_keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+
+-- If you select something in visual mode and then paste over it, the new text will overwrite your register. 
+--  This mapping will hold onto whatever is currently in your register so you can select something else and overwrite again
 vim.api.nvim_set_keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
