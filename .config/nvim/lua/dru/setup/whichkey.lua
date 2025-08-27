12a0b5ebb6f6507c31264aa85db428553fc7c78c
Alexandru Dumbrava <al.dumbrava@gmail.com>
2025-08-27 14:49:56 -0400

    Fix(which-key): Turned on help as that shows some missing bindings for some reason

--- .config/nvim/lua/dru/setup/whichkey.lua
+++ .config/nvim/lua/dru/setup/whichkey.lua
@@ -63,7 +63,7 @@ function M.config()
         -- },
         -- ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
         -- hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "^:", "^ ", "^call ", "^lua " }, -- hide mapping boilerplate
-        show_help = false, -- show a help message in the command line for using WhichKey
+        show_help = true, -- show a help message in the command line for using WhichKey
         show_keys = true, -- show the currently pressed key and its label as a message in the command line
         disable = {
             buftypes = {},
